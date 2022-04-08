1fdca32c206f4b72e982564d8a3b12240d96d725
Michael Getachew <mgetachew59@gmail.com>
2022-04-08 03:12:52 -0700

    task 0 Again

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,12 +8,12 @@
   * Return: Always 0 (Success/correct)
   */
 
-int main (void)
+int main(void)
 {
 int n;
 
 srand(time(0));
-n = rand() -RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 
 if (n==0)
 {
