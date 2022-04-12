bf761ec2e347eab84c07f33ecd79bb636d5a5f40
Michael Getachew <mgetachew59@gmail.com>
2022-04-12 14:18:30 -0700

    task 8,9,10,11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * print_t0_98 - prints all natural numbers from imput to 98.
+  *               in order separated by a comma followed by space
+  * @n: the number to be counting at.
+  */
+
+void print_t0_98(int n)
+
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, " n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, " n++);
+		printf("%d\n", n);
+	}
+}
