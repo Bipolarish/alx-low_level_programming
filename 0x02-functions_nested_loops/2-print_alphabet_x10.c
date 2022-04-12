ef8823fca1e2193c3bccdffb87d8fa9574b30af5
Michael Getachew <mgetachew59@gmail.com>
2022-04-12 13:08:55 -0700

    task 1,2,3..

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,10 @@
 void print_alphabet_x10(void)
 
 {
-	int count = 0;
+	int count;
 	char letter;
 
-	while (count++ <= 9)
+	for (count = 0; count < 10; count++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
