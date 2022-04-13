a5d716e00079cc46d17f042595cf4668a8ee7af2
Michael Getachew <mgetachew59@gmail.com>
2022-04-13 01:55:22 -0700

    task 1.......1.,,1...

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,9 +15,11 @@ int main(void)
 	/**
 	  * while (i < 10)
 	  * {
+	  * Infinite loop - No increase of variable
 	  *        putchar(i);
 	  * }
 	  */
 	printf("Infinite loop avoided! \\o/\n");
-        return (0);
+
+	return (0);
 }
