a5f2ef4da23b84cf0ad72c12b476ae312bf7e8ce
Michael Getachew <mgetachew59@gmail.com>
2022-04-14 11:44:04 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints the number zero to nine
+  *
+  * Return: always 0.
+  */
+
+void print_numbers(void)
+
+{
+	int i;
+
+	i = o;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
+
+	_putchar('\n');
+}
