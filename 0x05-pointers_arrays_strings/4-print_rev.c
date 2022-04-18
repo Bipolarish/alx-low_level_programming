54d05787babe03bf5a383f57ca79718140f6151a
Michael Getachew <mgetachew59@gmail.com>
2022-04-18 05:33:30 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,18 @@
 
 void print_rev(char *s)
 {
-	int i = 0;
-	int length;
+	int i;
 
-	for (length = 0; s[length] != '\0'; length++)
+	i = 0;
+	while (s[i] != '\0')
 	{
+		i++;
 	}
 
-	for (i = length - 1; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 	{
-		_putchar(s[i]);
+		_putchar (s[i]);
 	}
-	_putchar('\n');
+
+	_putchar ('\n');
 }
